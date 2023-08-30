968e6366a98ba659a63532aa20f6bdfd077aa94a
tuhana <git@tuhana.me>
2023-08-30 22:12:37 +0300

    Add descriptions to CLI flags

--- src/cli.rs
+++ src/cli.rs
@@ -6,12 +6,15 @@ pub struct Args {
     #[command(subcommand)]
     pub command: SubCommands,
 
+    /// Revolt session token
     #[arg(long, env = "LR_TOKEN", required = true)]
     pub token: String,
 
+    /// Status template to show when listening
     #[arg(long, env = "LR_STATUS_TEMPLATE", default_value_t = String::from("🎵 %ARTIST% – %NAME%"))]
     pub status_template: String,
 
+    /// Status to show when not listening
     #[arg(long, env = "LR_STATUS_IDLE")]
     pub status_idle: Option<String>,
 }
@@ -20,10 +23,13 @@ pub struct Args {
 pub enum SubCommands {
     #[cfg(feature = "lastfm")]
     LastFM {
+        /// Last.fm username
         #[arg(long, env = "LR_LASTFM_USER", required = true)]
         user: String,
+        /// Last.fm API key
         #[arg(long, env = "LR_LASTFM_API_KEY", required = true)]
         api_key: String,
+        /// Check interval
         #[arg(long, env = "LR_LASTFM_CHECK_DELAY", default_value_t = 6)]
         check_delay: u64,
     },
