f4704caa0997376c0a6751ba0e7f9ef5f1849a62
tuhana <tuhana.cat+git@gmail.com>
2024-08-05 18:23:05 +0300

    Partially implement `config revolt get-session-token`

--- src/cli/config.rs
+++ src/cli/config.rs
@@ -1,4 +1,6 @@
 use clap::Subcommand;
+use inquire::{validator::Validation, Password, Text};
+use regex::Regex;
 
 use super::Command;
 
@@ -6,7 +8,15 @@ use super::Command;
 pub enum CommandSubcommands {
     /// Generate an example lure configuration file and print it.
     Generate,
-    // TODO: Implement `revolt get-session-token`
+    /// Revolt commands for obtaining some configuration options.
+    #[command(subcommand)]
+    Revolt(RevoltSubcommands),
+}
+
+#[derive(Subcommand, Debug)]
+pub enum RevoltSubcommands {
+    /// Login to Revolt to obtain a new session token.
+    GetSessionToken,
 }
 
 impl Command for CommandSubcommands {
@@ -15,6 +25,46 @@ impl Command for CommandSubcommands {
             Self::Generate => {
                 print!("{}", include_str!("../../resources/config.example.yaml"));
             }
+            Self::Revolt(revolt_subcommand) => match *revolt_subcommand {
+                RevoltSubcommands::GetSessionToken => {
+                    let email = match Text::new("Revolt e-mail:")
+                        .with_placeholder("i@love.cat")
+                        .with_validator(|email: &str| {
+                            if Regex::new(r#"[^@ \t\r\n]+@[^@ \t\r\n]+\.[^@ \t\r\n]+"#)
+                                .expect("e-mail validation regex is somehow invalid now.")
+                                .is_match(email)
+                            {
+                                Ok(Validation::Valid)
+                            } else {
+                                Ok(Validation::Invalid(
+                                    "Entered e-mail address is invalid.".into(),
+                                ))
+                            }
+                        })
+                        .prompt()
+                    {
+                        Ok(email) => email,
+                        Err(_) => return Ok(()),
+                    };
+                    let password = match Password::new("Revolt password:")
+                        .with_validator(|password: &str| {
+                            if password.chars().count() > 0 {
+                                Ok(Validation::Valid)
+                            } else {
+                                Ok(Validation::Invalid("Password cannot be empty.".into()))
+                            }
+                        })
+                        .with_help_message("Your password will NOT be stored and will only be used to obtain a session token.")
+                        .without_confirmation()
+                        .prompt()
+                    {
+                        Ok(password) => password,
+                        Err(_) => return Ok(()),
+                    };
+
+                    dbg!(email, password);
+                }
+            },
         }
 
         Ok(())
