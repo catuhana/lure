b270c24ba160460b5494647539be59d46400faf7
tuhana <tuhana.cat+git@gmail.com>
2024-08-09 16:57:07 +0300

    Remove unused fields from models and use `String` instead of `Cow<'_, str>`

--- src/services/lastfm/models.rs
+++ src/services/lastfm/models.rs
@@ -1,80 +1,36 @@
-use serde::{Deserialize, Serialize};
-
 pub mod user {
-    use super::{Deserialize, Serialize};
+    use serde::Deserialize;
 
     pub mod get_recent_tracks {
-        use std::borrow::Cow;
+        use super::Deserialize;
 
-        use super::{Deserialize, Serialize};
-
-        #[derive(Deserialize, Serialize, Debug)]
-        pub struct Data<'a> {
-            pub recenttracks: RecentTracks<'a>,
+        #[derive(Deserialize, Debug)]
+        pub struct Data {
+            pub recenttracks: RecentTracks,
         }
 
-        #[derive(Deserialize, Serialize, Debug)]
-        pub struct RecentTracks<'a> {
-            pub track: Vec<Track<'a>>,
+        #[derive(Deserialize, Debug)]
+        pub struct RecentTracks {
+            pub track: Vec<Track>,
+        }
+
+        #[derive(Deserialize, Debug)]
+        pub struct Track {
+            pub artist: Artist,
+            pub name: String,
             #[serde(rename = "@attr")]
-            pub attr: RecentTracksAttr<'a>,
+            pub attr: Option<TrackAttr>,
         }
 
-        #[derive(Deserialize, Serialize, Debug)]
-        pub struct Track<'a> {
-            pub artist: Artist<'a>,
-            pub streamable: Cow<'a, str>,
-            pub image: Vec<Image<'a>>,
-            pub mbid: Cow<'a, str>,
-            pub album: Album<'a>,
-            pub name: Cow<'a, str>,
-            #[serde(rename = "@attr")]
-            pub attr: Option<TrackAttr<'a>>,
-            pub url: Cow<'a, str>,
-            pub date: Option<Date<'a>>,
-        }
-
-        #[derive(Deserialize, Serialize, Debug)]
-        pub struct Artist<'a> {
-            pub mbid: Cow<'a, str>,
+        #[derive(Deserialize, Debug)]
+        pub struct Artist {
             #[serde(rename = "#text")]
-            pub text: Cow<'a, str>,
+            pub text: String,
         }
 
-        #[derive(Deserialize, Serialize, Debug)]
-        pub struct Image<'a> {
-            pub size: Cow<'a, str>,
-            #[serde(rename = "#text")]
-            pub text: Cow<'a, str>,
-        }
-
-        #[derive(Deserialize, Serialize, Debug)]
-        pub struct Album<'a> {
-            pub mbid: Cow<'a, str>,
-            #[serde(rename = "#text")]
-            pub text: Cow<'a, str>,
-        }
-
-        #[derive(Deserialize, Serialize, Debug)]
-        pub struct TrackAttr<'a> {
-            pub nowplaying: Option<Cow<'a, str>>,
-        }
-
-        #[derive(Deserialize, Serialize, Debug)]
-        pub struct Date<'a> {
-            pub uts: Cow<'a, str>,
-            #[serde(rename = "#text")]
-            pub text: Cow<'a, str>,
-        }
-
-        #[derive(Deserialize, Serialize, Debug)]
-        #[serde(rename_all = "camelCase")]
-        pub struct RecentTracksAttr<'a> {
-            pub user: Cow<'a, str>,
-            pub total_pages: Cow<'a, str>,
-            pub page: Cow<'a, str>,
-            pub per_page: Cow<'a, str>,
-            pub total: Cow<'a, str>,
+        #[derive(Deserialize, Debug)]
+        pub struct TrackAttr {
+            pub nowplaying: Option<String>,
         }
     }
 }
--- src/services/listenbrainz/models.rs
+++ src/services/listenbrainz/models.rs
@@ -2,30 +2,28 @@ pub mod user {
     use serde::Deserialize;
 
     pub mod playing_now {
-        use std::borrow::Cow;
-
         use super::Deserialize;
 
         #[derive(Deserialize, Debug)]
-        pub struct Data<'a> {
-            pub payload: Payload<'a>,
+        pub struct Data {
+            pub payload: Payload,
         }
 
         #[derive(Deserialize, Debug)]
-        pub struct Payload<'a> {
-            pub listens: Vec<Listen<'a>>,
+        pub struct Payload {
+            pub listens: Vec<Listen>,
         }
 
         #[derive(Deserialize, Debug)]
-        pub struct Listen<'a> {
+        pub struct Listen {
             pub playing_now: bool,
-            pub track_metadata: TrackMetadata<'a>,
+            pub track_metadata: TrackMetadata,
         }
 
         #[derive(Deserialize, Debug)]
-        pub struct TrackMetadata<'a> {
-            pub artist_name: Cow<'a, str>,
-            pub track_name: Cow<'a, str>,
+        pub struct TrackMetadata {
+            pub artist_name: String,
+            pub track_name: String,
         }
     }
 }
