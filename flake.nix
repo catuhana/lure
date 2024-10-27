05af12cb22733fd0f828bb54b4219738c2aab9b5
tuhana <tuhana.cat+git@gmail.com>
2024-10-27 03:56:25 +0300

    Use rust-overlay cus fenix openssl issue

--- flake.nix
+++ flake.nix
@@ -5,22 +5,27 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-parts.url = "github:hercules-ci/flake-parts";
 
-    fenix = {
-      url = "github:nix-community/fenix";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = inputs@{ self, nixpkgs, flake-parts, fenix, ... }: flake-parts.lib.mkFlake { inherit inputs; }
+  outputs = inputs@{ self, nixpkgs, flake-parts, rust-overlay, ... }: flake-parts.lib.mkFlake { inherit inputs; }
     {
       systems = nixpkgs.lib.systems.flakeExposed;
 
       perSystem = { self', lib, pkgs, system, ... }:
         {
+          _module.args.pkgs = import inputs.nixpkgs {
+            inherit system;
+            overlays = [ (import rust-overlay) ];
+          };
+
           packages.default =
             let
               cargoTOML = lib.importTOML ./Cargo.toml;
-              toolchain = fenix.packages.${system}.minimal.toolchain;
+              toolchain = pkgs.rust-bin.stable.latest.minimal;
             in
             (pkgs.makeRustPlatform {
               cargo = toolchain;
