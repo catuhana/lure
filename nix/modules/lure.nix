570d2543c5b9cc324c5c117fd7038264dca31012
tuhana <tuhana.cat+git@gmail.com>
2024-10-27 15:11:42 +0300

    sakjdsakl

--- nix/modules/lure.nix
+++ nix/modules/lure.nix
@@ -161,8 +161,8 @@ in
         LoadCredential =
           let
             credentials = [ ]
-              ++ lib.optional (lib.isPath cfg.lastfm.api_key) "lastfm-api-key:${cfg.lastfm.api_key}"
-              ++ lib.optional (lib.isPath cfg.revolt.session_token) "revolt-session-token:${cfg.revolt.session_token}";
+              ++ lib.optional (lib.isPath cfg.services.lastfm.api_key) "lastfm-api-key:${cfg.lastfm.api_key}"
+              ++ lib.optional (lib.isPath cfg.services.revolt.session_token) "revolt-session-token:${cfg.revolt.session_token}";
           in
           credentials;
       };
